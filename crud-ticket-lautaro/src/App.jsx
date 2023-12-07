65603e6862a122bd98e11801b81ca75f13ca052a
Lautaro <lauta96aguero@gmail.com>
2023-12-07 19:53:42 -0300

    create-vite-app-ticket-form

---
+++ crud-ticket-lautaro/src/App.jsx
@@ -0,0 +1,14 @@
+import TicketForm from './components/TicketForm'
+import './App.css'
+import TicketTable from './components/TicketTable'
+
+function App() {
+  return (   
+    <div className="App">       
+      <TicketForm/>
+      <TicketTable/>
+    </div>
+  )
+}
+
+export default App
---
+++ crud-ticket-lautaro/src/components/TicketTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TicketTable() {
+  return (
+    <div>TicketTable</div>
+  )
+}
